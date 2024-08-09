78e3418b5e8db3f7606c95e2dfb202b6d732a8c8
SaeedoSunbul <saeedsunbul@outlook.com>
2024-08-09 06:48:33 +0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,45 @@
+function myFunction2(){
+
+    var arr = []
+    arr[0] = "https://w7.pngwing.com/pngs/604/326/png-transparent-dice-dice-1-image-file-formats-rectangle-dice-thumbnail.png"
+    arr[1] = "https://upload.wikimedia.org/wikipedia/commons/1/18/Dice-2.png"
+    arr[2] = "https://cdn.pixabay.com/photo/2013/07/12/17/39/dice-152175_1280.png"
+    arr[3] = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Dice-4.svg/600px-Dice-4.svg.png"
+    arr[4] = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStdtAlk6Ro_83B_fXPxPlolwgFvgZWFTT5Hsz6iZ-PHrR8RqI86QsZfJlhvaeUAC8g8GI&usqp=CAU"
+    arr[5] = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Dice-6.svg/600px-Dice-6.svg.png"
+   
+
+    //var rand1 = Math.floor(Math.random() * 6);
+    var rand2 = Math.floor(Math.random() * 6);
+ 
+   // var x = document.getElementById("img1")
+
+    var y = document.getElementById("img2")
+
+    
+    //if (x.src = "C:\Users\pc\Downloads\Dicee+Challenge+-+Starting+Files\images\dice1.png") {
+    //    x.src = arr[rand1];
+    //}
+    
+    if (y.src = "C:\Users\pc\Downloads\Dicee+Challenge+-+Starting+Files\images\dice1.png") {
+        y.src = arr[rand2];
+    }
+/*
+
+   
+    }
+    if (rand1 > rand2){
+        document.getElementById("player11").style.display="block"
+    }
+    if (rand1 < rand2) {
+        document.getElementById("player22").style.display="block"
+    }
+    if (rand1 === rand2) {
+        document.getElementById("Draw").style.display="block"
+    }  
+*/
+
+
+
+      
+}
